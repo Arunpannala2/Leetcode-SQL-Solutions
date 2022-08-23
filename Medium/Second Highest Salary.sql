03b5a0a9ef9434b5edd2556e26c959b0a5e5a438
Arun Pannala <98634240+Arunpannala2@users.noreply.github.com>
2022-08-22 19:54:22 -0700

    Create Second Highest Salary.sql

---
+++ Medium/Second Highest Salary.sql
@@ -0,0 +1,24 @@
+--Table: Employee
+
+--+-------------+------+
+--| Column Name | Type |
+--+-------------+------+
+--| id          | int  |
+--| salary      | int  |
+--+-------------+------+
+--id is the primary key column for this table.
+--Each row of this table contains information about the salary of an employee.
+ 
+--Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
+--The query result format is in the following example.
+
+--solution
+
+SELECT(
+SELECT distinct salary
+from employee
+order by salary desc
+limit 1 offset 1) as secondhighestsalary
+
+ 
