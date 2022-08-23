eea7715c5de92eae063e1d6d2df19941c2c7f73f
Arun Pannala <98634240+Arunpannala2@users.noreply.github.com>
2022-08-22 20:07:51 -0700

    Update Swap Salary.sql

--- Easy/Swap Salary.sql
+++ Easy/Swap Salary.sql
@@ -16,6 +16,7 @@
 --Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
 
 --Note that you must write a single update statement, do not write any select statement for this problem.
+--Solution 
 
 UPDATE Salary
 SET sex = (CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END)
